192577d65842d954bdf3884878fc115a633801b7
TimurP <timurpshinshev@gmail.com>
2025-08-05 14:15:32 +0300

    greatest change

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="app-footer">
-      <p>ИтмоПродМат001 | Кидайте свои офферы в тг @annoying_rusk</p>
+      <p>ИтмоПродМат001 | Кидайте свои офферы в тг @GridDen @nmuravya @annoying_rusk </p>
     </footer>
   );
 }
